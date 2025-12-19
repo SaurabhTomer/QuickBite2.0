f48ccee235ad8e88448fa3f6881472376ed18509
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-12-19 22:13:10 +0530

    signup and signin pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-
 import { Route , Routes } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 
+
+//backend url
+export const serverUrl = 'http://localhost:8000'
+
 function App() {
   return (
     <Routes>
-        <Route path='/signup' element={SignUp} />
-        <Route path='/signin' element={SignIn} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/signin' element={<SignIn/>} />
     </Routes>
   )
 }
