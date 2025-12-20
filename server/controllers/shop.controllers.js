fdc3ea1b58cb4d832a19ea74d8c5a86b7651b1e5
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-12-20 20:57:07 +0530

    getShopby city controller

--- server/controllers/shop.controllers.js
+++ server/controllers/shop.controllers.js
@@ -18,8 +18,8 @@ export const createEditShop = async (req, res) => {
     //find if shop exists or not of user
     let shop = await Shop.findOne({ owner: req.userId });
 
-    //if shop does not  exists for this user 
-    if ( !shop ) {
+    //if shop does not  exists for this user
+    if (!shop) {
       shop = await Shop.create({
         name,
         city,
@@ -51,19 +51,45 @@ export const createEditShop = async (req, res) => {
   }
 };
 
-
 //get my shop controller
-export const getMyShop=async (req,res) => {
-    try {
-        const shop=await Shop.findOne({owner:req.userId}).populate("owner").populate({
-            path:"items",
-            options:{sort:{updatedAt:-1}}
-        })
-        if(!shop){
-            return null
-        }
-        return res.status(200).json(shop)
-    } catch (error) {
-        return res.status(500).json({message:`get my shop error ${error}`})
+export const getMyShop = async (req, res) => {
+  try {
+    const shop = await Shop.findOne({ owner: req.userId })
+      .populate("owner")
+      .populate({
+        path: "items",
+        options: { sort: { updatedAt: -1 } },
+      });
+    if (!shop) {
+      return null;
     }
-}
+    return res.status(200).json(shop);
+  } catch (error) {
+    return res.status(500).json({ message: `get my shop error ${error}` });
+  }
+};
+
+//get all shop of city
+export const getShopByCity = async (req, res) => {
+  try {
+    //took city from param
+    const { city } = req.params;
+
+    // Match shops where the 'city' field matches the given city name
+    // ^${city}$  → ensures an exact match (no extra characters before or after)
+    // "i"        → makes the match case-insensitive (Delhi = delhi = DELHI)
+    const shops = await Shop.find({
+      city: { $regex: new RegExp(`^${city}$`, "i") },
+    }).populate("items");
+
+    // if shop not found
+    if (!shops) {
+      return res.status(400).json({ message: "shops not found" });
+    }
+
+    //response
+    return res.status(200).json(shops);
+  } catch (error) {
+    return res.status(500).json({ message: `get shop by city error ${error}` });
+  }
+};
