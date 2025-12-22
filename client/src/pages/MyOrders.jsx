c25aa07992b18ec8272c23c940c9cc8f630e2f6e
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-12-22 23:09:31 +0530

    add info

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -13,38 +13,21 @@ function MyOrders() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
-    useEffect(() => {
-        socket?.on('newOrder', (data) => {
-            if (data.shopOrders?.owner._id == userData._id) {
-                dispatch(setMyOrders([data, ...myOrders]))
-            }
-        })
-
-        socket?.on('update-status', ({ orderId, shopId, status, userId }) => {
-            if (userId == userData._id) {
-                dispatch(updateRealtimeOrderStatus({ orderId, shopId, status }))
-            }
-        })
-
-        return () => {
-            socket?.off('newOrder')
-            socket?.off('update-status')
-        }
-    }, [socket])
-
 
 
 
     return (
         <div className='"w-full min-h-screen bg-[#fff9f6] flex justify-center px-4'>
             <div className='w-full max-w-200 p-4'>
-
+            {/* back order and heading */}
                 <div className='flex items-center gap-5 mb-6 '>
                     <div className=' z-10 ' onClick={() => navigate("/")}>
                         <IoIosArrowRoundBack size={35} className='text-[#ff4d2d]' />
                     </div>
                     <h1 className='text-2xl font-bold  text-start'>My Orders</h1>
                 </div>
+
+        {/* show orders that you placed */}
                 <div className='space-y-6'>
                     {myOrders?.map((order, index) => (
                         userData.role == "user" ?
