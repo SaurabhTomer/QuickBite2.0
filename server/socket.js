da0e9e3b6d5ebe7f65a024a12e1e7af9f683bc5d
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-12-27 01:24:02 +0530

    socket setup when user online and offline

--- server/socket.js
+++ server/socket.js
@@ -1,16 +1,39 @@
 import User from "./models/user.model.js"
 
-export const socketHandler =  (io) => {
-    io.on('connection' , (socket) => {
-        socket.on('identity' , async(({userId}) => {
-            try {
-                const user = await User.findByIdAndUpdate(userId , {
-                    socketId:socket.id,
-                },
-            {new:true})
-            } catch (error) {
-                
-            }
-        }))
+export const socketHandler = (io) => {
+  // Run when a new client connects
+  io.on('connection', (socket) => {
+    console.log(socket.id) // Log socket id
+
+    // Save user socket id when user identifies
+    socket.on('identity', async ({ userId }) => {
+      try {
+        await User.findByIdAndUpdate(
+          userId,
+          {
+            socketId: socket.id, // store socket id
+            isOnline: true        // mark user online
+          },
+          { new: true }
+        )
+      } catch (error) {
+        console.log(error)
+      }
     })
+
+    // Run when client disconnects
+    socket.on('disconnect', async () => {
+      try {
+        await User.findOneAndUpdate(
+          { socketId: socket.id }, // find user by socket
+          {
+            socketId: null,        // remove socket id
+            isOnline: false        // mark user offline
+          }
+        )
+      } catch (error) {
+        console.log(error)
+      }
+    })
+  })
 }
